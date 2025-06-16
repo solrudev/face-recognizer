98c876b029ecfcf9b4af6fe953863cae54813850
Ilya Fomichev <fomil.music@gmail.com>
2025-06-16 13:13:44 +0500

    hide toolbar on face capture screen

--- app/src/main/kotlin/ru/solrudev/facerecognizer/ui/MainActivity.kt
+++ app/src/main/kotlin/ru/solrudev/facerecognizer/ui/MainActivity.kt
@@ -3,6 +3,9 @@ package ru.solrudev.facerecognizer.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.core.view.isVisible
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,6 +13,10 @@ import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.androidbroadcast.vbpd.viewBinding
 import dev.chrisbanes.insetter.applyInsetter
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import ru.solrudev.facerecognizer.R
 import ru.solrudev.facerecognizer.databinding.MainNavHostBinding
 
@@ -31,6 +38,12 @@ class MainActivity : AppCompatActivity(R.layout.main_nav_host) {
 			applyInsets()
 			toolbarNavHost.setupWithNavController(navController, appBarConfiguration)
 		}
+		navController.currentBackStackEntryFlow
+			.flowWithLifecycle(lifecycle)
+			.map { entry -> entry.destination.id == R.id.face_capture_fragment }
+			.distinctUntilChanged()
+			.onEach { isCameraScreen -> binding.appBarLayoutNavHost.isVisible = !isCameraScreen }
+			.launchIn(lifecycleScope)
 	}
 
 	private fun MainNavHostBinding.applyInsets() {
